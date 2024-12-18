e7a2102783cb0bc4d871d9e0f53d71554b00c7de
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2024-12-18 12:30:41 -0500

    dun da dun da dyn

---
+++ list.js
@@ -0,0 +1,25 @@
+const DUNDADUNDADUN = [
+  {
+    quote:
+      "this is so unhealthy that by the time you guys 13 your double chins will be touching your cankles",
+    season: "3",
+    episode: "28",
+  },
+  {
+    quote:
+      "oh mommy I want some chocolate. Im sorry little sausage but you've had too much candy this week. meh meh meh meh meh. *COMBUSTION* ",
+    season: "2",
+    episode: "30",
+  },
+  {
+    quote:
+      "darwin I can't believe you just did that! did what? I dont remember.",
+    season: "2",
+    episode: "21",
+  },
+  {
+    quote: "bobert terminate gumball or whatever",
+    season: "2",
+    episode: "50",
+  },
+];
